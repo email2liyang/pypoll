a183b8f01fafd478fe6a96f3c12bc7c5c918e9fe
ivan <email2liyang@gmail.com>
2013-08-22 19:44:40 +0800

    enable build context processor

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -159,3 +159,11 @@ LOGGING = {
         },
     }
 }
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.core.context_processors.debug',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.media',
+    'django.contrib.auth.context_processors.auth',
+    'django.core.context_processors.request',
+)
