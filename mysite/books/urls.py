881265b233c665980abb39dbd1a3c89712091b64
ivan <email2liyang@gmail.com>
2013-08-22 18:32:33 +0800

    url pattern prefix

--- mysite/books/urls.py
+++ mysite/books/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import patterns, url
 
-urlpatterns = patterns('',
-                       url(r'^hello/$', 'books.views.hello'),
-                       url(r'^meta/$', 'books.views.display_meta'),
-                       url(r'^search_form/$', 'books.views.search_form'),
-                       url(r'^search/$', 'books.views.search'),
-                       url(r'^contact/$', 'books.views.contact'),
-                       url(r'^contact/thanks/$', 'books.views.contact'),
+urlpatterns = patterns('books.views',
+                       url(r'^hello/$', 'hello'),
+                       url(r'^meta/$', 'display_meta'),
+                       url(r'^search_form/$', 'search_form'),
+                       url(r'^search/$', 'search'),
+                       url(r'^contact/$', 'contact'),
+                       url(r'^contact/thanks/$', 'contact'),
                        )
--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -5,9 +5,9 @@ from django.contrib import admin
 admin.autodiscover()
 
 
-urlpatterns = patterns('',
-                       url(r'^$', 'polls.views.index'),
-                       url(r'^(?P<poll_id>\d+)/$', 'polls.views.detail'),
-                       url(r'^(?P<poll_id>\d+)/results/$','polls.views.results'),
-                       url(r'^(?P<poll_id>\d+)/vote/$','polls.views.vote'),
+urlpatterns = patterns('polls.views',
+                       url(r'^$', 'index'),
+                       url(r'^(?P<poll_id>\d+)/$', 'detail'),
+                       url(r'^(?P<poll_id>\d+)/results/$','results'),
+                       url(r'^(?P<poll_id>\d+)/vote/$','vote'),
                        )
